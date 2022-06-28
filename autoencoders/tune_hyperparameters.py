40966be6b6cc10f8755cd4623b1ae10b1d675af4
egovorko <ekaterina.govorkova@cern.ch>
2022-06-28 15:54:37 +0200

    tune number of layers for the student

--- autoencoders/tune_hyperparameters.py
+++ autoencoders/tune_hyperparameters.py
@@ -7,15 +7,15 @@ import tensorflow as tf
 import argparse
 import setGPU
 
+from tensorflow import keras
+
+from tensorflow.keras import layers
 from tensorflow.keras.optimizers import Adam
 from kerastuner import HyperParameters
 from tensorboard import program
 from tensorflow.keras.callbacks import TensorBoard
 
-tracking_address = '/tmp/tb_logs' # the path of your log file.
-
-
-from models import student_model
+tracking_address = '/tmp/tb_logs' # the path of your log file for TensorBoard
 
 
 class HyperStudent(kerastuner.HyperModel):
@@ -27,15 +27,21 @@ class HyperStudent(kerastuner.HyperModel):
 
     def build(self, hp):
 
-        filters = hp.Choice('filters', [4, 8, 16, 32], default=4)
+        inputs = keras.Input(shape=self.input_shape[1:])
+        x = layers.Flatten()(inputs)
+        # Number of layers of the MLP is a hyperparameter.
+        for i in range(hp.Int('mlp_layers', 2, 4)):
+            # Number of units of each layer are
+            # different hyperparameters with different names.
+            output_node = layers.Dense(
+                units=hp.Choice(f'units_{i}', [4, 8, 16, 32], default=32),
+                activation='relu',
+            )(x)
 
-        hyper_student = student_model(
-            self.input_shape,
-            filters,
-            quant_size=0,
-            dropout=None,
-            expose_latent=False
-            )
+        # The last layer contains 1 unit, which
+        # represents the learned loss value
+        outputs = layers.Dense(units=1, activation='relu')(x)
+        hyper_student = keras.Model(inputs=inputs, outputs=outputs)
 
         hyper_student.compile(
             optimizer=Adam(lr=3E-3, amsgrad=True),
@@ -43,6 +49,7 @@ class HyperStudent(kerastuner.HyperModel):
             )
 
         return hyper_student
+
 
 def optimisation(input_file, distillation_loss):
 
@@ -57,14 +64,14 @@ def optimisation(input_file, distillation_loss):
           objective='val_loss',
           max_trials=10,
           overwrite=True,
-          directory='hyper_tuning',
+          directory='output/hyper_tuning',
           )
     tuner.search_space_summary()
     tuner.search(
         x=x_train,
         y=y_train,
-        epochs=5,
-        validation_split=0.2,
+        epochs=10,
+        validation_split=0.5,
         # Use the TensorBoard callback.
         # The logs will be write to "/tmp/tb_logs".
         callbacks=[TensorBoard(tracking_address)]
@@ -73,20 +80,13 @@ def optimisation(input_file, distillation_loss):
     tuner.results_summary()
     logging.info('Get the optimal hyperparameters')
 
-    best_hps = tuner.get_best_hyperparameters(num_trials=10)[0]
+    best_hps = tuner.get_best_hyperparameters(num_trials=20)[0]
 
     logging.info('Getting and printing best hyperparameters!')
     print(best_hps)
 
 
 if __name__ == '__main__':
-
-    # tb = program.TensorBoard()
-    # tb.configure(argv=[None, '--logdir', tracking_address])
-    # url = tb.launch()
-    # print(f"Tensorflow listening on {url}")
-
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--input-file', type=str, help='input file', required=True)
     parser.add_argument('--distillation-loss', type=str, default='mse', help='Loss to use for distillation')
